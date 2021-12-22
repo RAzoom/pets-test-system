5541410c5d446164fe8fd9bef9789df07556dec3
ARazumovskiy <Arazumovskiy@bivgroup.com>
2021-12-22 18:51:30 +0300

    add serializer for localDate add new fields to return ExhibitionResponse

--- registration-request-service/src/main/java/razoom/controller/pojo/ExhibitionResponse.java
+++ registration-request-service/src/main/java/razoom/controller/pojo/ExhibitionResponse.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+import razoom.utils.jackson.date.LocalDateSerializer;
 import razoom.utils.jackson.encrypt.CipherLongSerializer;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,4 +18,20 @@ public class ExhibitionResponse {
 
     @Schema(description = "Наименование выставки", example = "Супер-турбо")
     private String name;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @Schema(description = "Дана проведения", example = "2022-07-06")
+    private LocalDate startExhibition;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @Schema(description = "Дана завершения", example = "2014-07-06")
+    private LocalDate finishExhibition;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @Schema(description = "Дана начала регистрации", example = "2022-06-06")
+    private LocalDate startRegistration;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @Schema(description = "Дана проведения", example = "2022-07-05")
+    private LocalDate finishRegistration;
 }
